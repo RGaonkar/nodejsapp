9823c5efd998224a6933e83ee76ccfbdf65e6a28
Saumil <saumil@saumil-System-Product-Name.(none)>
2014-12-02 20:48:38 +0530

    Sample file

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');  //add the http module
+
+var myServer = http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type" : "text/html"});
+	response.write("<b>Hello</b> World");
+	response.end();
+});
+
+myServer.listen(3000);
+console.log("Go to http://localhost:3000");
