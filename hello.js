00dbdc0512cc0e55558b2d2a2b58bbaebe4f7ae3
Saumil <saumil@saumil-System-Product-Name.(none)>
2014-12-03 01:21:45 +0530

    first message

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');  //add the http module
 
 var myServer = http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type" : "text/html"});
-	response.write("<b>Hello</b> World");
+	response.write("<b>Hello new</b> World");
 	response.end();
 });
 
